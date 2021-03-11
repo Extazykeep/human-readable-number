b2c8af2fa45c272061a05c267c8dc02997ef0f9d
Extazykeep <52160853+Extazykeep@users.noreply.github.com>
2021-03-11 12:22:42 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
+const numbers = ["zero",'one','two','three','four','five','six','seven','eight','nine',"ten","eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen"];
+const digit = ["","","twenty","thirty","forty","fifty","sixty","seventy","eighty","ninety"];
+let str = "";
+
 module.exports = function toReadable (number) {
-  let numbers = ["zero",'one','two','three','four','five','six','seven','eight','nine',"ten",
-                   "eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen"];
-    const digit = ["","","twenty","thirty","forty","fifty","sixty","seventy","eighty","ninety"];
-    let str = "";
     if(number < 20){
       lessthantwenty(number)
     }  
@@ -12,14 +12,16 @@ module.exports = function toReadable (number) {
     if(number > 99 && number <1000){
       lessthanthousand(number)
     }
-  function lessthantwenty(num) {
-       for(let i = 0;i < numbers.length;i++){
-       if(i === num){
-         str+=numbers[i]
-        }
-       }
+  return str
 }
-  function lessthanhundred(num){
+function lessthantwenty(num) {
+    for(let i = 0;i < numbers.length;i++){
+       if(i === num){
+            str+=numbers[i]
+        }
+    }
+}
+function lessthanhundred(num){
       let splited = num.toString().split("")
       if(splited[1] === "0"){
         for(let i = 0; i < digit.length; i++){
@@ -40,22 +42,20 @@ module.exports = function toReadable (number) {
             str+=numbers[i]
           }
         }
-      }
+   }
 }
 function lessthanthousand(num){
   let splited = num.toString().split("");
+  let forlessthanhundred = +(splited[1] + splited[2])
   if(splited[1] === "0" && splited[2] === "0") {    
     lessthantwenty(+splited[0])
       str+= " " + "hundred"
     return
-  }
-  let forlessthanhundred = +(splited[1] + splited[2])
+  }  
   lessthantwenty(+splited[0]);
   str+= " " + "hundred" + " ";
   if(forlessthanhundred > 19){
       lessthanhundred(forlessthanhundred)
   }
   else lessthantwenty(forlessthanhundred)
-}
-  return str
 }
